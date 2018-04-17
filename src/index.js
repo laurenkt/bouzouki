ce049e7303f346b3444d7fcd71f5a5eda1b9d450
Lauren <lauren@tomasello.me>
2018-04-17 09:22:39 +0100

    Add bibliography

--- src/index.js
+++ src/index.js
@@ -5,11 +5,21 @@ import Bouzouki from './Bouzouki';
 import UI from './UI';
 
 document.addEventListener('DOMContentLoaded', _ => {
+	// Word Count
 	document.querySelector('footer').innerText =
 		Array.prototype.map.call(document.querySelectorAll('section > *'), el => 
-			el.innerText.split(/\s+/mi).length).reduce((a, b) => a+b) + ' words';
+			el.innerText.split(/\s+/mi).length).reduce((a, b) => a+b) + ' / 2000 words';
 
+	// References
+	document.querySelectorAll('section > h1').forEach(el => {
+		if (el.innerText !== 'References')
+			return;
 
+		const references = Array.prototype.map.call(document.querySelectorAll('a[title]'), (el, idx) =>
+			`<p>${idx + 1} ${el.getAttribute('title')}</p>`).join();
+
+		el.insertAdjacentHTML('afterend', references);
+	})
 
 	let root = document.createElement('div');
 
